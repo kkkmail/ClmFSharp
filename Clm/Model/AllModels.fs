df01da488ccc36b39b9950d43050482d1d012d25
kkkmail <kkk@jfcglobal.net>
2018-12-27 04:36:37 -0500

    FSV:1.4.0.0, V:1.4.0.0

--- Clm/Model/AllModels.fs
+++ Clm/Model/AllModels.fs
@@ -10,3 +10,103 @@ module AllModels =
     /// !!! This file grows automatically at the end. Do not modify it without extreme need !!!
     let allModelData : list<ModelDataParams> = 
         []
+
+        @
+        [
+            {
+                modelInfo = 
+                    {
+                        fileStructureVersionNumber = "1.4.0.0"
+                        versionNumber = "1.4.0.0"
+                        seedValue = 172447908
+                        modelName = "20181227_001"
+                        numberOfSubstances = 87
+                        numberOfAminoAcids = NumberOfAminoAcids.TwoAminoAcids
+                        maxPeptideLength = MaxPeptideLength.ThreeMax
+                        updateAllModels = true
+                        allResultsFile = @"C:\GitHub\ClmFSharp\Clm\Clm\..\Model\AllResults.fs"
+                    }
+
+                allParams = 
+                    [
+                        {
+                            synthesisDistribution = DeltaDistribution(524849207, { threshold = None; scale = None; shift = None }) |> Delta
+                            forwardScale = Some 0.001
+                            backwardScale = Some 0.001
+                        }
+                        |> SynthRndParam
+                        |> SynthesisRateParam
+
+                        {
+                            catSynthRndEeParams = 
+                            {
+                                rateMultiplierDistr = TriangularDistribution(951347499, { threshold = Some 0.002; scale = Some 10000.0; shift = None }) |> Triangular |> RateMultDistr
+                                eeForwardDistribution = SymmetricTriangularDistribution(891228166, { threshold = None; scale = None; shift = None }) |> SymmetricTriangular |> EeDistribution |> Some
+                                eeBackwardDistribution = SymmetricTriangularDistribution(1117587636, { threshold = None; scale = None; shift = None }) |> SymmetricTriangular |> EeDistribution |> Some
+                            }
+                        }
+                        |> CatSynthRndParam
+                        |> CatalyticSynthesisRateParam
+
+                        {
+                            destructionDistribution = DeltaDistribution(272909746, { threshold = None; scale = None; shift = None }) |> Delta
+                            forwardScale = Some 0.001
+                            backwardScale = Some 0.001
+                        }
+                        |> DestrRndParam
+                        |> DestructionRateParam
+
+                        {
+                            catDestrRndEeParams = 
+                            {
+                                rateMultiplierDistr = TriangularDistribution(394793438, { threshold = Some 0.002; scale = Some 10000.0; shift = None }) |> Triangular |> RateMultDistr
+                                eeForwardDistribution = SymmetricTriangularDistribution(319154248, { threshold = None; scale = None; shift = None }) |> SymmetricTriangular |> EeDistribution |> Some
+                                eeBackwardDistribution = SymmetricTriangularDistribution(1363247578, { threshold = None; scale = None; shift = None }) |> SymmetricTriangular |> EeDistribution |> Some
+                            }
+                        }
+                        |> CatDestrRndParam
+                        |> CatalyticDestructionRateParam
+
+                        {
+                            wasteRecyclingRate = 0.1
+                        }
+                        |> WasteRecyclingRateParam
+
+                        {
+                            aminoAcids = AminoAcid.getAminoAcids NumberOfAminoAcids.TwoAminoAcids
+                            simBaseDistribution = UniformDistribution(1264831739, { threshold = Some 0.3; scale = None; shift = None }) |> Uniform
+                            getRateMultiplierDistr = DeltaRateMultDistrGetter
+                            getForwardEeDistr = DeltaEeDistributionGetter
+                            getBackwardEeDistr = DeltaEeDistributionGetter
+                        }
+                        |> CatSynthSimParam
+                        |> CatalyticSynthesisRateParam
+
+                        {
+                            aminoAcids = AminoAcid.getAminoAcids NumberOfAminoAcids.TwoAminoAcids
+                            simBaseDistribution = UniformDistribution(151488329, { threshold = Some 0.3; scale = None; shift = None }) |> Uniform
+                            getRateMultiplierDistr = DeltaRateMultDistrGetter
+                            getForwardEeDistr = DeltaEeDistributionGetter
+                            getBackwardEeDistr = DeltaEeDistributionGetter
+                        }
+                        |> CatDestrSimParam
+                        |> CatalyticDestructionRateParam
+
+                        {
+                            ligationDistribution = DeltaDistribution(1278074175, { threshold = None; scale = None; shift = None }) |> Delta
+                            forwardScale = Some 1.0
+                            backwardScale = Some 1.0
+                        }
+                        |> LigRndParam
+                        |> LigationRateParam
+
+                        {
+                            sedimentationDirectDistribution = TriangularDistribution(1900354787, { threshold = Some 1E-05; scale = None; shift = None }) |> Triangular
+                            forwardScale = Some 10000.0
+                        }
+                        |> SedDirRndParam
+                        |> SedimentationDirectRateParam
+
+                    ]
+            }
+        ]

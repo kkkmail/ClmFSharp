376d52f558a8dbfbeabad3685010669ef0a80f16
kkkmail <kkk@jfcglobal.net>
2018-12-28 18:37:01 -0500

    FSV:1.5.0.0, V:1.5.0.0

--- Clm/ContGen/Builder.fs
+++ Clm/ContGen/Builder.fs
@@ -1,22 +1,40 @@
 ﻿namespace ContGen
 
-open Fake.DotNet
-open Fake.Core
-open Fake.IO
-open Fake.IO.Globbing.Operators //enables !! and globbing
+//open Fake.DotNet
+//open Fake.Core
+//open Fake.IO
+//open Fake.IO.Globbing.Operators //enables !! and globbing
 
 module Builder = 
-    // Properties
-    let buildDir = "./build/"
+    //// Properties
+    //let buildDir = "./build/"
 
-    // Targets
-    Target.create "Clean" (fun _ ->
-      Shell.cleanDir buildDir
-    )
+    //// Targets
+    //Target.create "Clean" (fun _ ->
+    //  Shell.cleanDir buildDir
+    //)
 
-    Target.create "BuildApp" (fun _ ->
-      !! "src/app/**/*.csproj"
-        |> MSBuild.runRelease id buildDir "Build"
-        |> Trace.logItems "AppBuild-Output: "
-    )
+    //Target.create "BuildApp" (fun _ ->
+    //  !! "src/app/**/*.csproj"
+    //    |> MSBuild.runRelease id buildDir "Build"
+    //    |> Trace.logItems "AppBuild-Output: "
+    //)
+
+
+    type X () = 
+
+        let loadParams() : unit = failwith ""
+
+        let generateModel() : unit = failwith ""
+
+        let saveModel() : unit = failwith ""
+
+        let compile() : unit = failwith ""
+
+        let copyToExecLocation() : unit = failwith ""
+
+        let runModel() : unit = failwith ""
+
+
+        member fake.sucks() = 0
 
--- Clm/ContGen/ContGenTest.fsx
+++ Clm/ContGen/ContGenTest.fsx
@@ -1,4 +1,18 @@
 ﻿#r @"C:\GitHub\ClmFSharp\Clm\packages\FAKE.5.8.4\tools\FakeLib.dll"
+#r @"C:\GitHub\ClmFSharp\Clm\packages\FAKE.5.8.4\tools\System.Reactive.dll"
+
+open System.IO
+
+
+//let execContext = Fake.Core.Context.FakeExecutionContext.Create false "path/to/script.fsx" []
+//Fake.Core.Context.setExecutionContext (Fake.Core.Context.RuntimeContext.Fake execContext)
+//let execContext = Fake.Core.Context.FakeExecutionContext.Create false (Path.Combine(__SOURCE_DIRECTORY__, __SOURCE_FILE__)) []
+//Fake.Core.Context.setExecutionContext (Fake.Core.Context.RuntimeContext.Fake execContext)
+
+#if !FAKE
+let execContext = Fake.Core.Context.FakeExecutionContext.Create false "build.fsx" []
+Fake.Core.Context.setExecutionContext (Fake.Core.Context.RuntimeContext.Fake execContext)
+#endif
 
 open Fake.DotNet
 open Fake.Core
@@ -7,6 +21,8 @@ open Fake.IO.Globbing.Operators //enables !! and globbing
 
 // Properties
 let buildDir = @"C:\Temp\WTF\"
+
+printfn "1"
 
 // Targets
 Target.create "Clean" (fun _ ->
@@ -20,16 +36,19 @@ Target.create "BuildApp" (fun _ ->
     |> Trace.logItems "AppBuild-Output: "
 )
 
-//Target.create "Default" (fun _ ->
-//  Trace.trace "Hello World from FAKE"
-//)
+Target.create "Default" (fun _ ->
+  Trace.trace "Hello World from FAKE"
+)
 
-//open Fake.Core.TargetOperators
+printfn "2"
 
-//"Clean"
-//  ==> "BuildApp"
-//  ==> "Default"
+open Fake.Core.TargetOperators
 
+"Clean"
+  ==> "BuildApp"
+  ==> "Default"
+
+printfn "3"
 // start build
 Target.runOrDefault "Default"
 

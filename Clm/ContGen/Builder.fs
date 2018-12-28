676e1819d7f9aacdb86d8a50d289bc9771140252
kkkmail <kkk@jfcglobal.net>
2018-12-28 17:16:12 -0500

    FSV:1.5.0.0, V:1.5.0.0

---
+++ Clm/ContGen/Builder.fs
@@ -0,0 +1,22 @@
+﻿namespace ContGen
+
+open Fake.DotNet
+open Fake.Core
+open Fake.IO
+open Fake.IO.Globbing.Operators //enables !! and globbing
+
+module Builder = 
+    // Properties
+    let buildDir = "./build/"
+
+    // Targets
+    Target.create "Clean" (fun _ ->
+      Shell.cleanDir buildDir
+    )
+
+    Target.create "BuildApp" (fun _ ->
+      !! "src/app/**/*.csproj"
+        |> MSBuild.runRelease id buildDir "Build"
+        |> Trace.logItems "AppBuild-Output: "
+    )
+
---
+++ Clm/ContGen/ContGenTest.fsx
@@ -0,0 +1,35 @@
+﻿#r @"C:\GitHub\ClmFSharp\Clm\packages\FAKE.5.8.4\tools\FakeLib.dll"
+
+open Fake.DotNet
+open Fake.Core
+open Fake.IO
+open Fake.IO.Globbing.Operators //enables !! and globbing
+
+// Properties
+let buildDir = @"C:\Temp\WTF\"
+
+// Targets
+Target.create "Clean" (fun _ ->
+  Shell.cleanDir buildDir
+)
+
+Target.create "BuildApp" (fun _ ->
+  //!! "src/app/**/*.csproj"
+  !! @"..\SolverRunner\SolverRunner.fsproj"
+    |> MSBuild.runRelease id buildDir "Build"
+    |> Trace.logItems "AppBuild-Output: "
+)
+
+//Target.create "Default" (fun _ ->
+//  Trace.trace "Hello World from FAKE"
+//)
+
+//open Fake.Core.TargetOperators
+
+//"Clean"
+//  ==> "BuildApp"
+//  ==> "Default"
+
+// start build
+Target.runOrDefault "Default"
+

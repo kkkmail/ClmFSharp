fe5d996e602e0c7470326a411d9fa1993180f711
kkkmail <kkk@jfcglobal.net>
2018-12-29 08:53:23 -0500

    FSV:1.5.0.0, V:1.5.0.0

---
+++ Clm/ContGen/DatabaseTypes.fs
@@ -0,0 +1,47 @@
+﻿namespace ContGen
+
+open System.Data
+open System.Data.SqlClient
+open FSharp.Data
+open Microsoft.SqlServer.Types
+
+open Configuration
+open DynamicSql
+
+module DatabaseTypes = 
+
+    type ClmDB = SqlProgrammabilityProvider<ClmSqlProviderName, ConfigFile = AppConfigFile>
+
+
+    type SystemSettingTable = ClmDB.dbo.Tables.SystemSetting
+    type SystemSettingTableRow = SystemSettingTable.Row
+    type SystemSettingTableData = SqlCommandProvider<"select * from dbo.SystemSetting", ClmConnectionString, ResultType.DataReader>
+
+
+    let openConnIfClosed (conn : SqlConnection) = 
+        match conn.State with 
+        | ConnectionState.Closed -> do conn.Open()
+        | _ -> ignore ()
+
+
+    let loadSettings (connStr : string) =
+        use conn : SqlConnection = new SqlConnection (connStr)
+        conn.Open()
+
+        let systemSettingTable = new SystemSettingTable()
+        (new SystemSettingTableData(conn)).Execute() |> systemSettingTable.Load
+
+        systemSettingTable.Rows
+        |> List.ofSeq
+        |> List.groupBy (fun e -> e.SettingName)
+        |> List.map (fun (e, v) -> e, v |> List.groupBy (fun r -> r.SettingOrderId) |> Map.ofList)
+        |> Map.ofList
+
+
+    //let loadSettings (conn : SqlConnection) = 
+    //    openConnIfClosed conn
+    //    use data = new SystemSettingTableData(conn)
+    //    use reader = new DynamicSqlDataReader(data.Execute())
+    //    [ while reader.Read() do yield (reader?City, reader?CityOriginalName) |> City.tryCreate ]
+
+
--- Clm/ContGen/Settings.fs
+++ Clm/ContGen/Settings.fs
@@ -3,10 +3,90 @@ open System.Data.SqlClient
 open FSharp.Data
 
 open Configuration
+open Clm.ReactionRates
 
 module Settings = 
 
-    type ClmDB = SqlProgrammabilityProvider<ClmSqlProviderName, ConfigFile = AppConfigFile>
+    type ParamSetting<'T> = 
+        {
+            isUsed : bool
+            paramValue : 'T
+        }
 
-    type Setting = 
-        | X of int
+
+////===========================================================
+//let foodModel = ReactionRateProvider.defaultFoodCreationModel 0.01
+//let wasteModel = ReactionRateProvider.defaultWasteRemovalModel 10.0
+//let wasteRecyclingModel = ReactionRateProvider.defaultWasteRecyclingModel 0.1
+////===========================================================
+//let synthModel = ReactionRateProvider.defaultSynthRndModel rnd (0.001, 0.001)
+////let catSynthRndParams = (synthModel, (Some 0.02), 1000.0)
+//let catSynthRndParams = (synthModel, (Some 0.002), 10000.0)
+////let catSynthRndParams = (synthModel, (Some 0.0005), 1000.0)
+////let catSynthModel = ReactionRateProvider.defaultCatSynthRndModel rnd catSynthRndParams
+//let catSynthModel = ReactionRateProvider.defaultCatSynthSimModel rnd catSynthRndParams (Some 0.3, numberOfAminoAcids)
+////===========================================================
+//let destrModel = ReactionRateProvider.defaultDestrRndModel rnd (0.001, 0.001)
+////let catDestrRndParams = (destrModel, (Some 0.02), 1000.0)
+//let catDestrRndParams = (destrModel, (Some 0.002), 10000.0)
+////let catDestrRndParams = (destrModel, (Some 0.0005), 1000.0)
+////let catDestrModel = ReactionRateProvider.defaultCatDestrRndModel rnd catDestrRndParams
+//let catDestrModel = ReactionRateProvider.defaultCatDestrSimModel rnd catDestrRndParams (Some 0.3, numberOfAminoAcids)
+////===========================================================
+////let ligModel = ReactionRateProvider.defaultLigRndModel rnd (0.001, 0.0001)
+////let ligModel = ReactionRateProvider.defaultLigRndModel rnd (1.0, 0.1)
+//let ligModel = ReactionRateProvider.defaultLigRndModel rnd (1.0, 1.0)
+//let catLigModel = ReactionRateProvider.defaultCatLigRndModel rnd (ligModel, (Some 0.00005), 2000.0)
+////===========================================================
+//let sedDirModel = ReactionRateProvider.defaultSedDirRndModel rnd (0.00001, 10000.0)
+//let sedAllModel = ReactionRateProvider.defaultSedAllRndModel rnd 0.1
+////===========================================================
+//let racemModel = ReactionRateProvider.defaultRacemRndModel rnd 0.001
+////let catRacemRndParams = (racemModel, (Some 0.02), 1000.0)
+//let catRacemRndParams = (racemModel, (Some 0.0005), 1000.0)
+////let catRacemModel = ReactionRateProvider.defaultCatRacemRndModel rnd catRacemRndParams
+//let catRacemModel = ReactionRateProvider.defaultCatRacemSimModel rnd catRacemRndParams (Some 0.2, numberOfAminoAcids)
+////===========================================================
+//let rates = 
+//    [
+//        //foodModel |> FoodCreationRateModel
+//        //wasteModel |> WasteRemovalRateModel
+//        wasteRecyclingModel |> WasteRecyclingRateModel
+
+//        synthModel |> SynthesisRateModel
+//        catSynthModel |> CatalyticSynthesisRateModel
+
+//        destrModel |> DestructionRateModel
+//        catDestrModel |> CatalyticDestructionRateModel
+
+//        ligModel |> LigationRateModel
+//        //catLigModel |> CatalyticLigationRateModel
+
+//        sedDirModel |> SedimentationDirectRateModel
+//        //sedAllModel |> SedimentationAllRateModel
+
+//        //racemModel |> RacemizationRateModel
+//        //catRacemModel |> CatalyticRacemizationRateModel
+//    ]
+////===========================================================
+//let modelGenerationParams = 
+//    {
+//        fileStructureVersionNumber = FileStructureVersionNumber
+//        versionNumber = VersionNumber
+//        seedValue = Some seed
+//        numberOfAminoAcids = numberOfAminoAcids
+//        maxPeptideLength = maxPeptideLength
+//        reactionRateModels = rates
+//        updateFuncType = UseFunctions
+//        modelLocationData = ModelLocationInputData.defaultValue
+//        updateAllModels = updateAllModels
+//    }
+////===========================================================
+
+
+
+    type Settings = 
+        {
+            foodModel : ParamSetting<FoodCreationParam>
+        }
+

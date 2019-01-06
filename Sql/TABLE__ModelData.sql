1c881ae2b01ae841ea7f9cdfdbccb3cb968ea253
kkkmail <kkk@jfcglobal.net>
2019-01-05 19:26:49 -0500

    FSV:1.5.0.0, V:1.5.0.0

--- Sql/TABLE__ModelData.sql
+++ Sql/TABLE__ModelData.sql
@@ -1,19 +1,19 @@
-CREATE TABLE dbo.ModelData(
-	modelDataId bigint IDENTITY(1,1) NOT NULL,
-	numberOfAminoAcids int NOT NULL,
-	maxPeptideLength int NOT NULL,
-	seedValue int NULL,
-	fileStructureVersion nvarchar(50) NOT NULL,
-	modelData nvarchar(max) NOT NULL,
-	createdOn datetime NOT NULL,
- CONSTRAINT PK_ModelData PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[ModelData](
+	[modelDataId] [bigint] IDENTITY(1,1) NOT NULL,
+	[numberOfAminoAcids] [int] NOT NULL,
+	[maxPeptideLength] [int] NOT NULL,
+	[seedValue] [int] NULL,
+	[fileStructureVersion] [nvarchar](50) NOT NULL,
+	[modelData] [nvarchar](max) NOT NULL,
+	[createdOn] [datetime] NOT NULL,
+ CONSTRAINT [PK_ModelData] PRIMARY KEY CLUSTERED 
 (
-	modelDataId ASC
+	[modelDataId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON PRIMARY
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
-ALTER TABLE dbo.ModelData ADD  CONSTRAINT DF_ModelData_createdOn  DEFAULT (getdate()) FOR createdOn
+ALTER TABLE [dbo].[ModelData] ADD  CONSTRAINT [DF_ModelData_createdOn]  DEFAULT (getdate()) FOR [createdOn]
 GO
 
 

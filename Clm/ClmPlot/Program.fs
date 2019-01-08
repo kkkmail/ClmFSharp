6f77d5b3e149e73312dcd3f44460d2a4c629d8f9
kkkmail <kkk@jfcglobal.net>
2019-01-08 11:01:52 -0500

    FSV:1.5.0.0, V:1.5.0.4

--- Clm/ClmPlot/Program.fs
+++ Clm/ClmPlot/Program.fs
@@ -5,11 +5,16 @@ open ClmPlot.PlotTasks
 
 [<EntryPoint>]
 let main argv =
-    let parser = ArgumentParser.Create<ClmPlotArguments>(programName = "ClmPlot.exe")
-    let results = parser.Parse argv
+    try
+        let parser = ArgumentParser.Create<ClmPlotArguments>(programName = "ClmPlot.exe")
+        let results = parser.Parse argv
 
-    match results.GetAllResults() |> ClmPlotTask.tryCreate with
-    | Some task -> task.run()
-    | None ->
-        printfn "%s" (parser.PrintUsage())
-        -1
+        match results.GetAllResults() |> ClmPlotTask.tryCreate with
+        | Some task -> task.run()
+        | None ->
+            printfn "%s" (parser.PrintUsage())
+            -1
+    with
+        | exn ->
+            printfn "%s" exn.Message
+            -1
--- Clm/ContGen/Program.fs
+++ Clm/ContGen/Program.fs
@@ -4,12 +4,16 @@ open Argu
 
 [<EntryPoint>]
 let main argv =
-    let parser = ArgumentParser.Create<ContGenArguments>(programName = "ContGen.exe")
-    let results = parser.Parse argv
+    try
+        let parser = ArgumentParser.Create<ContGenArguments>(programName = "ContGen.exe")
+        let results = parser.Parse argv
 
-    match results.GetAllResults() |> ContGenTask.tryCreate with
-    | Some task -> task.run()
-    | None ->
-        printfn "%s" (parser.PrintUsage())
-        -1
-
+        match results.GetAllResults() |> ContGenTask.tryCreate with
+        | Some task -> task.run()
+        | None ->
+            printfn "%s" (parser.PrintUsage())
+            -1
+    with
+        | exn ->
+            printfn "%s" exn.Message
+            -1

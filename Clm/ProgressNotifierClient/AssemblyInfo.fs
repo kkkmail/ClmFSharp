00563e0dfffa82fdd40c9f033a5a29bf8dcf03ef
kkkmail <kkk@jfcglobal.net>
2019-01-11 09:41:40 -0500

    FSV:1.6.0.0, V:1.6.0.0

--- Clm/ProgressNotifierClient/AssemblyInfo.fs
+++ Clm/ProgressNotifierClient/AssemblyInfo.fs
@@ -1,8 +1,8 @@
 ﻿namespace ProgressNotifierClient.AssemblyInfo
 
 open System.Reflection
-open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
+open ClmSys.VersionInfo
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("ProgressNotifierClient")>]
-[<assembly: AssemblyCopyright("Copyright ©  2019")>]
+[<assembly: AssemblyCopyright(CopyrightInfo)>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -23,19 +23,8 @@ open System.Runtime.InteropServices
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("6779f3df-43d1-40cd-9c30-91b1f3a92a1f")>]
-
-// Version information for an assembly consists of the following four values:
-//
-//       Major Version
-//       Minor Version
-//       Build Number
-//       Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion(VersionNumber)>]
+[<assembly: AssemblyFileVersion(VersionNumber)>]
 
 do
     ()

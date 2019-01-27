e3cc3ff1b47ea9da2fb93305c27b329e3735ad9c
kkkmail <kkk@jfcglobal.net>
2019-01-27 03:12:05 -0500

    FSV:1.6.0.1, V:1.6.0.6

--- Clm/ContGenService/WindowsService.fs
+++ Clm/ContGenService/WindowsService.fs
@@ -9,24 +9,29 @@ open ContGenServiceInfo.ServiceInfo
 
 module WindowsService =
 
+    let startServiceRun logger =
+        try
+            let channel = new Tcp.TcpChannel (ContGenServicePort)
+            ChannelServices.RegisterChannel (channel, false)
+
+            RemotingConfiguration.RegisterWellKnownServiceType
+                ( typeof<ContGenService>, ContGenServiceName, WellKnownObjectMode.Singleton )
+        with
+            | e ->
+                logger e
+                ignore()
+
+
     type public ContGenWindowsService () =
         inherit ServiceBase (ServiceName = ContGenServiceName)
 
         let initService () = ()
         do initService ()
+        let logger e = ignore()
 
         override service.OnStart (args:string[]) =
             base.OnStart(args)
-
-            try
-                let channel = new Tcp.TcpChannel (ContGenServicePort)
-                ChannelServices.RegisterChannel (channel, false)
-
-                RemotingConfiguration.RegisterWellKnownServiceType
-                    ( typeof<ContGenService>, ContGenServiceName, WellKnownObjectMode.Singleton )
-            with
-                | e ->
-                    ignore()
+            startServiceRun logger
 
         override service.OnStop () =
             base.OnStop()

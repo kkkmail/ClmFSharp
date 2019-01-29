d4844c30a38086bc2ba6b18be359f8285b0dcbc0
kkkmail <kkk@jfcglobal.net>
2019-01-29 02:01:31 -0500

    FSV:1.6.0.2, V:1.6.0.8

--- Clm/ClmGenerator/NoOfSubstAndReact.fsx
+++ Clm/ClmGenerator/NoOfSubstAndReact.fsx
@@ -31,30 +31,52 @@ let getInfo maxPeptideLength numberOfAminoAcids =
     let chiralAminoAcids = ChiralAminoAcid.getAminoAcids numberOfAminoAcids
     let peptides = Peptide.getPeptides maxPeptideLength numberOfAminoAcids
     let allChains = (chiralAminoAcids |> List.map (fun a -> [ a ])) @ (peptides |> List.map (fun p -> p.aminoAcids))
+    let allLigChains = allChains |> List.filter(fun a -> a.Length < maxPeptideLength.length)
 
-    let allChainsSeq = allChains |> Seq.ofList
+    //let allChainsSeq = allChains |> Seq.ofList
 
-    let allPairsSeq =
-        Seq.allPairs allChainsSeq allChainsSeq
-        |> Seq.map (fun (a, b) -> orderPairs (a, b))
-        |> Seq.filter (fun (a, _) -> a.Head.isL)
-        |> Seq.distinct
+    //let allPairsSeq =
+    //    Seq.allPairs allChainsSeq allChainsSeq
+    //    |> Seq.map (fun (a, b) -> orderPairs (a, b))
+    //    |> Seq.filter (fun (a, _) -> a.Head.isL)
+    //    |> Seq.distinct
 
-    let ligationPairs =
-        allPairsSeq
-        |> Seq.filter (fun (a, b) -> a.Length + b.Length <= maxPeptideLength.length)
-        |> Seq.toList
+    //let ligationPairs =
+    //    allPairsSeq
+    //    |> Seq.filter (fun (a, b) -> a.Length + b.Length <= maxPeptideLength.length)
+    //    |> Seq.toList
+
+    let ligationPairs2 =
+        List.allPairs allLigChains allLigChains
+        |> List.filter (fun (a, b) -> a.Length + b.Length <= maxPeptideLength.length)
+        |> List.map (fun (a, b) -> orderPairs (a, b))
+        |> List.filter (fun (a, _) -> a.Head.isL)
+        |> List.distinct
 
     printfn "numberOfAminoAcids = %A" numberOfAminoAcids
     printfn "aminoAcids.Length = %A" aminoAcids.Length
     printfn "chiralAminoAcids.Length = %A" chiralAminoAcids.Length
     printfn "peptides.Length = %A" peptides.Length
     printfn "allChains.Length = %A" allChains.Length
-    printfn "ligationPairs.Length = %A" ligationPairs.Length
+    printfn "allLigChains.Length = %A" allLigChains.Length
+    //printfn "ligationPairs.Length = %A" ligationPairs.Length
+    printfn "ligationPairs2.Length = %A" ligationPairs2.Length
+    //printfn "(ligationPairs = ligationPairs2) = %A" (ligationPairs = ligationPairs2)
     printfn "\n***\n"
 
 let a =
     [
+        OneAminoAcid
+        TwoAminoAcids
+        ThreeAminoAcids
+        FourAminoAcids
+        FiveAminoAcids
+        SixAminoAcids
+        SevenAminoAcids
+        EightAminoAcids
+        NineAminoAcids
+        TenAminoAcids
+
         ElevenAminoAcids
         TwelveAminoAcids
         ThirteenAminoAcids
@@ -62,10 +84,11 @@ let a =
         FifteenAminoAcids
         SixteenAminoAcids
 
-        //SeventeenAminoAcids
-        //EighteenAminoAcids
-        //NineteenAminoAcids
-        //TwentyAminoAcids
+        SeventeenAminoAcids
+        EighteenAminoAcids
+        NineteenAminoAcids
+        TwentyAminoAcids
+
         //TwentyOneAminoAcids
         //TwentyTwoAminoAcids
         //TwentyThreeAminoAcids

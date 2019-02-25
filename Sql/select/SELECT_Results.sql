8853a629420e29156279cbb6f057ae9bbb6d61a8
kkkmail <kkk@jfcglobal.net>
2019-02-25 00:19:00 -0500

    FSV:2.0.0.0, V:2.0.0.2

--- Sql/select/SELECT_Results.sql
+++ Sql/select/SELECT_Results.sql
@@ -2,41 +2,18 @@ USE [Clm]
 GO
 
 SELECT [resultDataId]
-      ,[modelDataId]
-      ,[numberOfAminoAcids]
-      ,[maxPeptideLength]
-
+      ,r.[modelDataId]
+	  ,m.defaultSetIndex
       ,[y0]
       ,[tEnd]
       ,[useAbundant]
-
       ,[maxEe]
       ,[maxAverageEe]
-      ,[createdOn]
-
-      --,[aminoAcids]
-      --,[allSubst]
-      --,[allInd]
-      --,[allRawReactions]
-      --,[allReactions]
-      --,[x]
-      --,[t]
-FROM [dbo].[ResultData]
-where 
-	--[modelDataId] >= 26 and
-	modelDataId in (select modelDataId from ModelData where defaultSetIndex = 4)
-	and numberOfAminoAcids = 11
-	and maxEe > 0.0001
-
-
-
-select distinct modelDataId
-from ResultData
-where 
-	--modelDataId >= 26 and
-	modelDataId in (select modelDataId from ModelData where defaultSetIndex = 4)
-	and numberOfAminoAcids = 11
-	and maxEe > 0.0001
-
+	  ,m.createdOn as modelCreatedOn
+      ,r.[createdOn]
+  FROM [dbo].[ResultData] r inner join ModelData m on r.modelDataId = m.modelDataId
+  --where modelDataId >= 35
+  order by modelDataId, y0
+GO
 
 

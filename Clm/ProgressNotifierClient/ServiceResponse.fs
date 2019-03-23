e7cadbde4221c1f28ca98d75744b274db8eaaf87
kkkmail <kkk@jfcglobal.net>
2019-03-22 20:44:55 -0400

    FSV:2.1.0.0, V:2.1.0.1

--- Clm/ProgressNotifierClient/ServiceResponse.fs
+++ Clm/ProgressNotifierClient/ServiceResponse.fs
@@ -4,16 +4,16 @@ open System
 open ContGenServiceInfo.ServiceInfo
 
 module ServiceResponse =
-    type ResponseHandler () =
-        let url = "tcp://localhost:" + (ContGenServicePort.ToString()) + "/" + ContGenServiceName
-        let service : IContGenService = Activator.GetObject (typeof<IContGenService>, url) :?> IContGenService
 
-        //member this.progressNotifierService : IProgressNotifier = service :> IProgressNotifier
+    type ResponseHandler (address, port) =
+        let url = getServiceUrlImpl address port ContGenServiceName
+        let service = Activator.GetObject (typeof<IContGenService>, url) :?> IContGenService
+
         member this.progressNotifierService : IContGenService = service
 
-        static member tryCreate() =
+        static member tryCreate address port =
             try
-                ResponseHandler () |> Some
+                ResponseHandler (address, port) |> Some
             with
                 | exn ->
                     printfn "Exception occurred: %s." exn.Message

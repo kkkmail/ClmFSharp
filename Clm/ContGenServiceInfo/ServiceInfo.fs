a49aee02119b7a02fed7b046139f68b8c2a9a091
kkkmail <kkk@jfcglobal.net>
2019-04-12 19:54:02 -0400

    FSV:3.0000, V:3.0.0.0

--- Clm/ContGenServiceInfo/ServiceInfo.fs
+++ Clm/ContGenServiceInfo/ServiceInfo.fs
@@ -130,7 +130,6 @@ module ServiceInfo =
             let state = service.getState()
             printfn "...state at %A =\n%s\n\n" DateTime.Now (state.ToString())
             if state.queue.Length = 0 then service.startGenerate()
-            service.loadQueue()
         else
             printfn "Not getting state at %A because callCount = %A." DateTime.Now callCount
             ignore()

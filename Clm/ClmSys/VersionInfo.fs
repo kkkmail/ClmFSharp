e3ce9ac5858bb80fc80615cac8d182e267cc42d7
kkkmail <kkk@jfcglobal.net>
2019-04-12 20:25:45 -0400

    FSV:3.0000, V:3.0.0.0

--- Clm/ClmSys/VersionInfo.fs
+++ Clm/ClmSys/VersionInfo.fs
@@ -3,7 +3,7 @@
 module VersionInfo =
 
     /// Increment fractional part by 0.0001, e.g. 1.0000 -> 1.0001 if an updated version can read the previous version format.
-    /// Increment integer part, e.g. 1.0000 -> 2.000, when the changes are completely incompatible with previous version.
+    /// Increment integer part, e.g. 1.0000 -> 2.0000, when the changes are completely incompatible with previous version.
     let FileStructureVersion = 3.0000m
 
 

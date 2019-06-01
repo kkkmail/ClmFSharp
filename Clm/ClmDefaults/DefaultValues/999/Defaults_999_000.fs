c592e12f4af02141528fbf424d40e9e73dfd2d32
kkkmail <kkk@jfcglobal.net>
2019-06-01 08:13:45 -0400

    FSV:3.0000, V:3.1.0.0

--- Clm/ClmDefaults/DefaultValues/999/Defaults_999_000.fs
+++ Clm/ClmDefaults/DefaultValues/999/Defaults_999_000.fs
@@ -8,6 +8,7 @@ module Defaults_999_000 =
 
     let clmDefaultValueId = 999_000L |> ClmDefaultValueId
     let description = Some "For n = 3 - 5"
+    let catRateGenType = ByEnantiomerPairs
 
     let defaultRateParams =
         //===========================================================
@@ -17,16 +18,16 @@ module Defaults_999_000 =
         //===========================================================
         let synthParam = ReactionRateProviderParams.defaultSynthRndParamImpl (0.001, 0.001)
         let catSynthRndParam = (synthParam, (Some 0.02), 10_000.0)
-        //let catSynthParam = ReactionRateProviderParams.defaultCatSynthRndParam catSynthRndParam
-        let catSynthParam = ReactionRateProviderParams.defaultCatSynthSimParam catSynthRndParam (Some 0.3)
+        //let catSynthParam = ReactionRateProviderParams.defaultCatSynthRndParam catSynthRndParam catRateGenType
+        let catSynthParam = ReactionRateProviderParams.defaultCatSynthSimParam catSynthRndParam (Some 0.3) catRateGenType
         //===========================================================
         let destrParam = ReactionRateProviderParams.defaultDestrRndParamImpl (0.001, 0.001)
         let catDestrRndParam = (destrParam, (Some 0.02), 100_000.0)
-        //let catDestrParam = ReactionRateProviderParams.defaultCatDestrRndParam catDestrRndParam
-        let catDestrParam = ReactionRateProviderParams.defaultCatDestrSimParam catDestrRndParam (Some 0.20)
+        //let catDestrParam = ReactionRateProviderParams.defaultCatDestrRndParam catDestrRndParam catRateGenType
+        let catDestrParam = ReactionRateProviderParams.defaultCatDestrSimParam catDestrRndParam (Some 0.20) catRateGenType
         //===========================================================
         let ligParam = ReactionRateProviderParams.defaultLigRndParamImpl (1.0, 1.0)
-        let catLigParam = ReactionRateProviderParams.defaultCatLigRndParam (ligParam, (Some 0.005), 2_000.0)
+        let catLigParam = ReactionRateProviderParams.defaultCatLigRndParam (ligParam, (Some 0.005), 2_000.0) catRateGenType
         //===========================================================
         // For n = ?
         let sedDirRndParam = (Some 0.002, 1000.0)
@@ -43,8 +44,8 @@ module Defaults_999_000 =
         //===========================================================
         let racemParam = ReactionRateProviderParams.defaultRacemRndParamImpl 0.001
         let catRacemRndParam = (racemParam, (Some 0.02), 1_000.0)
-        //let catRacemParam = ReactionRateProviderParams.defaultCatRacemRndParam catRacemRndParam
-        let catRacemParam = ReactionRateProviderParams.defaultCatRacemSimParam catRacemRndParam (Some 0.2)
+        //let catRacemParam = ReactionRateProviderParams.defaultCatRacemRndParam catRacemRndParam catRateGenType
+        let catRacemParam = ReactionRateProviderParams.defaultCatRacemSimParam catRacemRndParam (Some 0.2) catRateGenType
         //===========================================================
         let rates =
             [

b662a0bbf9e516c46497171ec977c03704b1eacf
kkkmail <kkk@jfcglobal.net>
2019-06-26 20:23:33 -0400

    FSV:3.0000, V:3.2.0.0

---
+++ Sql/!previousVersions/3100/-build.bat
@@ -0,0 +1,4 @@
+md .\!All
+del .\!All\all.sql
+
+copy /b .\tables\*.sql .\!All\all.sql
---
+++ Sql/!previousVersions/3100/tables/TABLE__CommandLineParam.sql
@@ -0,0 +1,27 @@
+IF OBJECT_ID('[dbo].[CommandLineParam]') IS NULL begin
+	print 'Creating table [dbo].[CommandLineParam] ...'
+
+	CREATE TABLE [dbo].[CommandLineParam](
+		commandLineParamId [uniqueidentifier] NOT NULL,
+		[clmTaskId] [uniqueidentifier] NOT NULL,
+		[y0] [money] NOT NULL,
+		[tEnd] [money] NOT NULL,
+		[useAbundant] [bit] NOT NULL DEFAULT ((0)),
+		[createdOn] datetime not null default ((getdate())),
+	 CONSTRAINT [PK_TCommandLineParam] PRIMARY KEY CLUSTERED 
+	(
+		commandLineParamId ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+
+	ALTER TABLE [dbo].[CommandLineParam]  WITH CHECK ADD  CONSTRAINT [FK_CommandLineParam_ClmTask] FOREIGN KEY([clmTaskId])
+	REFERENCES [dbo].ClmTask ([clmTaskId])
+
+	ALTER TABLE [dbo].[CommandLineParam] CHECK CONSTRAINT [FK_CommandLineParam_ClmTask]
+end else begin
+	print 'Table [dbo].[CommandLineParam] already exists ...'
+end
+go
+
+
+

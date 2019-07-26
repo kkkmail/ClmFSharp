1d6d17d81035799cd924fbd9b305ccad043f6f29
kkkmail <kkk@jfcglobal.net>
2019-07-26 19:28:23 -0400

    FSV:3.0000, V:4.0.0.0

--- Clm/MessageClient/Client.fs
+++ Clm/MessageClient/Client.fs
@@ -11,10 +11,10 @@ module Client =
         static member create() = Guid.NewGuid() |> MessageId
 
 
-    type MessageRecipient =
-        | MessageRecipient of Guid
+    type NodeId =
+        | NodeId of Guid
 
-        member this.value = let (MessageRecipient v) = this in v
+        member this.value = let (NodeId v) = this in v
 
 
     type MessageDeliveryType =
@@ -24,7 +24,7 @@ module Client =
 
     type MessageInfo<'T> =
         {
-            recipient : MessageRecipient
+            recipient : NodeId
             deliveryType : MessageDeliveryType
             messageData : 'T
         }
@@ -38,7 +38,6 @@ module Client =
     type Message<'T> =
         {
             messageId : MessageId
-            messageType : MessageType
             messageInfo : MessageInfo<'T>
             createdOn : DateTime
         }
@@ -46,8 +45,8 @@ module Client =
 
     type MessageClientProxy<'T> =
         {
-            loadMessages : unit -> List<Message<'T>>
-            saveMessage : Message<'T> -> unit
+            loadMessages : unit -> List<MessageType * Message<'T>>
+            saveMessage : MessageType -> Message<'T> -> unit
             deleteMessage : MessageId -> unit
             sendMessages : List<Message<'T>> -> List<Message<'T>>
             receiveMessages : unit -> List<Message<'T>>
@@ -62,15 +61,16 @@ module Client =
 
     type MessageClientState<'T> =
         {
+            messageClientData : MessageClientData<'T>
             incomingMessages : List<Message<'T>>
             outgoingMessages : List<Message<'T>>
         }
 
-        /// kk:20190726 - Removing this __ makes F# compiler fail on type MessageClient<'T> with:
-        /// This code is not sufficiently generic. The type variable 'T could not be generalized because it would escape its scope.
-        /// WTF
-        static member defaultValue __ =
+        /// kk:20190726 - Removing d makes F# compiler fail on type MessageClient<'T> with:
+        /// "This code is not sufficiently generic. The type variable 'T could not be generalized because it would escape its scope.". WTF!!!
+        static member defaultValue d =
             {
+                messageClientData = d
                 incomingMessages = []
                 outgoingMessages = []
             }
@@ -85,8 +85,10 @@ module Client =
 
     type MessageClient<'T>(d : MessageClientData<'T>) =
         let onStart s =
-            let messages = d.messageClientProxy.loadMessages()
-            let (incoming, outgoing) = messages |> List.partition (fun e -> match e.messageType with | IncomingMessage -> true | OutgoingMessage -> false)
+            let messages = s.messageClientData.messageClientProxy.loadMessages()
+            let (i, o) = messages |> List.partition (fun e -> match fst e with | IncomingMessage -> true | OutgoingMessage -> false)
+            let incoming = i |> List.map snd
+            let outgoing = o |> List.map snd
             { s with outgoingMessages = s.outgoingMessages @ outgoing; incomingMessages = s.incomingMessages @ incoming }
 
 
@@ -94,13 +96,12 @@ module Client =
             let message =
                 {
                     messageId = MessageId.create()
-                    messageType = OutgoingMessage
                     messageInfo = m
                     createdOn = DateTime.Now
                 }
 
             match m.deliveryType with
-            | GuaranteedDelivery -> d.messageClientProxy.saveMessage message
+            | GuaranteedDelivery -> s.messageClientData.messageClientProxy.saveMessage OutgoingMessage message
             | NonGuaranteedDelivery -> ignore()
 
             { s with outgoingMessages = message :: s.outgoingMessages }
@@ -109,7 +110,7 @@ module Client =
         let onGetMessages s =
             s.incomingMessages
             |> List.filter (fun e -> match e.messageInfo.deliveryType with | GuaranteedDelivery -> true | NonGuaranteedDelivery -> false)
-            |> List.map (fun e -> d.messageClientProxy.deleteMessage e.messageId)
+            |> List.map (fun e -> s.messageClientData.messageClientProxy.deleteMessage e.messageId)
             |> ignore
 
             { s with incomingMessages = [] }
@@ -119,10 +120,9 @@ module Client =
             let result =
                 s.outgoingMessages
                 |> List.rev
-                |> List.map (fun e -> { e with messageType = IncomingMessage })
-                |> d.messageClientProxy.sendMessages
+                |> s.messageClientData.messageClientProxy.sendMessages
 
-            let messages = d.messageClientProxy.receiveMessages()
+            let messages = s.messageClientData.messageClientProxy.receiveMessages()
             let transmitted = result |> List.map (fun e -> e.messageId) |> Set.ofList
             let outgoing = s.outgoingMessages |> List.map (fun e -> e.messageId) |> Set.ofList
             let failed = Set.difference outgoing transmitted
@@ -152,3 +152,113 @@ module Client =
         member __.getMessages() =
             let state = messageLoop.PostAndReply GetMessages
             state.incomingMessages |> List.rev
+
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+
+    type MessageServerProxy<'T> =
+        {
+            loadMessages : unit -> List<Message<'T>>
+            saveMessage : Message<'T> -> unit
+            deleteMessage : MessageId -> unit
+            //sendMessages : List<Message<'T>> -> List<Message<'T>>
+            //receiveMessages : unit -> List<Message<'T>>
+        }
+
+
+    type MessageServerData<'T> =
+        {
+            messageServerProxy : MessageServerProxy<'T>
+        }
+
+
+    type MessageServerState<'T> =
+        {
+            messageServerData : MessageServerData<'T>
+            messages : Map<NodeId, List<Message<'T>>>
+        }
+
+        static member defaultValue d =
+            {
+                messageServerData = d
+                messages = Map.empty
+            }
+
+
+    type MessageServerMessage<'T> =
+        | Start
+        | SendMessage of Message<'T>
+        | GetMessages of AsyncReplyChannel<MessageServerState<'T>>
+        //| TransmitMessages
+
+
+    type MessageServer<'T>(d : MessageServerData<'T>) =
+        let onStart s =
+            //let messages = s.messageClientData.messageClientProxy.loadMessages()
+            //let (incoming, outgoing) = messages |> List.partition (fun e -> match e.messageType with | IncomingMessage -> true | OutgoingMessage -> false)
+            //{ s with outgoingMessages = s.outgoingMessages @ outgoing; incomingMessages = s.incomingMessages @ incoming }
+            failwith ""
+
+
+        let onSendMessage s (m : Message<'T>) =
+            match m.messageInfo.deliveryType with
+            | GuaranteedDelivery -> s.messageServerData.messageServerProxy.saveMessage m
+            | NonGuaranteedDelivery -> ignore()
+
+            match s.messages.TryFind m.messageInfo.recipient with
+            | Some r -> { s with messages = s.messages.Add (m.messageInfo.recipient, m :: r) }
+            | None -> { s with messages = s.messages.Add (m.messageInfo.recipient, [ m ]) }
+
+
+        let onGetMessages s =
+            //s.incomingMessages
+            //|> List.filter (fun e -> match e.messageInfo.deliveryType with | GuaranteedDelivery -> true | NonGuaranteedDelivery -> false)
+            //|> List.map (fun e -> s.messageClientData.messageClientProxy.deleteMessage e.messageId)
+            //|> ignore
+
+            //{ s with incomingMessages = [] }
+            s
+
+
+        //let onTransmitMessages s =
+        //    let result =
+        //        s.outgoingMessages
+        //        |> List.rev
+        //        |> List.map (fun e -> { e with messageType = IncomingMessage })
+        //        |> s.messageClientData.messageClientProxy.sendMessages
+
+        //    let messages = s.messageClientData.messageClientProxy.receiveMessages()
+        //    let transmitted = result |> List.map (fun e -> e.messageId) |> Set.ofList
+        //    let outgoing = s.outgoingMessages |> List.map (fun e -> e.messageId) |> Set.ofList
+        //    let failed = Set.difference outgoing transmitted
+        //    let remaining = s.outgoingMessages |> List.filter (fun e -> failed.Contains e.messageId)
+        //    { s with outgoingMessages = remaining; incomingMessages = messages @ s.incomingMessages }
+
+
+        let messageLoop =
+            MailboxProcessor.Start(fun u ->
+                let rec loop (s : MessageServerState<'T> ) =
+                    async
+                        {
+                            match! u.Receive() with
+                            | Start -> return! onStart s |> loop
+                            | SendMessage m -> return! onSendMessage s m |> loop
+                            | GetMessages r ->
+                                r.Reply s
+                                return! onGetMessages s |> loop
+                            //| TransmitMessages -> return! onTransmitMessages s |> loop
+                        }
+
+                onStart (MessageServerState<'T>.defaultValue d) |> loop
+                )
+
+        member __.sendMessage m = SendMessage m |> messageLoop.Post
+
+        member __.getMessages (n : NodeId) =
+            //let state = GetMessages n |> messageLoop.PostAndReply
+            //state.incomingMessages |> List.rev
+            0
+

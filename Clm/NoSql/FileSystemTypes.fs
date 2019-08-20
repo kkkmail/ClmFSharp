33dab9e694421d74b53ce15224dadf22063457b4
kkkmail <kkk@jfcglobal.net>
2019-08-20 16:02:52 -0400

    FSV:3.0000, V:4.0.0.0

--- Clm/NoSql/FileSystemTypes.fs
+++ Clm/NoSql/FileSystemTypes.fs
@@ -51,23 +51,28 @@ module FileSystemTypes =
         if File.Exists f then File.Delete f
 
 
-    let tryGetX<'A> tableName (creator : string -> 'A) =
+    let getObjectIds<'A> tableName (creator : string -> 'A) =
         let folder = getFolderName tableName
         Directory.GetFiles(folder, "*." + storageExt)
-        |> Array.map (fun e -> Path.GetFileName e)
+        |> Array.map (fun e -> Path.GetFileNameWithoutExtension e)
         |> Array.map creator
+        |> List.ofArray
 
 
     let saveModelDataFs (m : ModelData) = saveData<ModelData, Guid> modelDataTblName m.modelDataId.value m
     let tryLoadModelDataFs (ModelDataId modelDataId) = tryLoadData<ModelData, Guid> modelDataTblName modelDataId
     let tryDeleteModelDataFs (ModelDataId modelDataId) = tryDeleteData<ModelData, Guid> modelDataTblName modelDataId
+    let getModelDataIdsFs () = getObjectIds<Guid> modelDataTblName Guid.Parse |> List.map ModelDataId
 
     let saveWorkerNodeRunModelDataFs (m : WorkerNodeRunModelData) = saveData<WorkerNodeRunModelData, Guid> workerNodeRunModelDataTblName m.remoteProcessId.value m
     let tryLoadWorkerNodeRunModelDataFs (RemoteProcessId processId) = tryLoadData<WorkerNodeRunModelData, Guid> workerNodeRunModelDataTblName processId
     let tryDeleteWorkerNodeRunModelDataFs (RemoteProcessId processId) = tryDeleteData<WorkerNodeRunModelData, Guid> workerNodeRunModelDataTblName processId
+    let getWorkerNodeRunModelDataIdsFs () = getObjectIds<Guid> workerNodeRunModelDataTblName Guid.Parse |> List.map RemoteProcessId
 
     let saveResultDataFs (r : ResultDataWithId) = saveData<ResultDataWithId, Guid> resultDataTblName r.resultDataId.value r
     let tryLoadResultDataFs (ResultDataId resultDataId) = tryLoadData<ResultDataWithId, Guid> resultDataTblName resultDataId
+    let tryDeleteResultDataFs (ResultDataId resultDataId) = tryDeleteData<WorkerNodeRunModelData, Guid> resultDataTblName resultDataId
+    let getResultDataIdsFs () = getObjectIds<Guid> resultDataTblName Guid.Parse |> List.map ResultDataId
 
     let saveChartsFs (ResultDataId resultDataId) (p : list<string>) = saveData<ChartInfo, Guid> chartTblName resultDataId { chartFileNames = p |> Array.ofList }
 
@@ -75,5 +80,6 @@ module FileSystemTypes =
         tryLoadData<ChartInfo, Guid> chartTblName resultDataId
         |> Option.bind (fun e -> e.chartFileNames |> List.ofArray |> Some)
 
-    let tryDeleteCharts (ResultDataId resultDataId) = tryDeleteData<ChartInfo, Guid> chartTblName resultDataId
+    let tryDeleteChartsFs (ResultDataId resultDataId) = tryDeleteData<ChartInfo, Guid> chartTblName resultDataId
+    let getChartsIdsFs () = getObjectIds<Guid> chartTblName Guid.Parse |> List.map ResultDataId
 

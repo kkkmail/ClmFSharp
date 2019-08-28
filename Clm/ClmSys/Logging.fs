574bd3841b64286db0441de6528b0b1b58ead5d4
kkkmail <kkk@jfcglobal.net>
2019-08-28 17:48:40 -0400

    FSV:3.0000, V:4.0.0.0

--- Clm/ClmSys/Logging.fs
+++ Clm/ClmSys/Logging.fs
@@ -1,6 +1,49 @@
 ﻿namespace ClmSys
 
+open System
+open System.Diagnostics
+
 module Logging =
+
+    type LogInfo =
+        {
+            Message : string
+            Date : DateTime
+        }
+
+
+    type ErrorInfo =
+        {
+            Error : exn
+            StackTrace : StackTrace
+        }
+
+
+    type LogMessage =
+        | Debug of LogInfo
+        | Info of LogInfo
+        | Warning of LogInfo
+        | Error of ErrorInfo * LogInfo
+        | Fatal of ErrorInfo * LogInfo
+
+        member this.Message =
+            match this with | Debug i | Info i | Warning i | Error (_, i) | Fatal (_, i) -> i.Message
+        member this.Exception =
+            match this with | Error (e, _) | Fatal (e, _) -> Some e.Error | _ -> None
+        member this.Level =
+            match this with | Debug _ -> Level.Debug | Info _ -> Level.Info | Warning _ -> Level.Warn | Error _ -> Level.Error | Fatal _ -> Level.Fatal
+        member this.LogInfo =
+            match this with | Debug i | Info i | Warning i | Error (_, i) | Fatal (_, i) -> i
+
+
+    let logAgent = MailboxProcessor.Start <| fun inbox ->
+        let rec logLoop () = async {
+            let! (message : LogMessage) = inbox.Receive()
+            writeLog message.Level message.LogInfo.Message message.Exception message.LogInfo.Date message.LogInfo.StackTrace
+            return! logLoop()
+        }
+        logLoop ()
+
 
     type Logger =
         {

dbba284d5108903a572f124f02cabfad52306e3a
kkkmail <kkk@jfcglobal.net>
2019-09-06 04:10:35 -0400

    FSV:3.0000, V:4.0.0.0

---
+++ Commands/clmCommands_014t1.bat
@@ -0,0 +1,9 @@
+ContGenAdm.exe add -i 15008 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15016 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15017 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15027 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15028 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15029 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15033 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15034 -n 10 -m 3 -y 10 -t 10000 -r 10
+ContGenAdm.exe add -i 15035 -n 10 -m 3 -y 10 -t 10000 -r 10

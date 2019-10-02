e15ca23fa6750419acc885b793c72ca0e5a16582
kkkmail <kkk@jfcglobal.net>
2019-10-02 06:30:19 -0400

    FSV:3.0000, V:4.0.0.3

---
+++ Commands/clmCommands_018.bat
@@ -0,0 +1,23 @@
+ContGenAdm.exe add -i 7004 -n 20 -m 3 -y 10 -t 250000 -r 36
+ContGenAdm.exe add -i 7002 -n 20 -m 3 -y 10 -t 250000 -r 28
+
+ContGenAdm.exe add -i 7001 -n 20 -m 3 -y 10 -t 250000 -r 4
+ContGenAdm.exe add -i 7006 -n 20 -m 3 -y 10 -t 250000 -r 36
+ContGenAdm.exe add -i 7005 -n 20 -m 3 -y 10 -t 250000 -r 36
+ContGenAdm.exe add -i 7007 -n 20 -m 3 -y 10 -t 250000 -r 68
+
+ContGenAdm.exe add -i 10004 -n 20 -m 3 -y 10 -t 250000 -r 36
+ContGenAdm.exe add -i 10001 -n 20 -m 3 -y 10 -t 250000 -r 28
+ContGenAdm.exe add -i 10000 -n 20 -m 3 -y 10 -t 250000 -r 29
+ContGenAdm.exe add -i 10002 -n 20 -m 3 -y 10 -t 250000 -r 17
+ContGenAdm.exe add -i 10003 -n 20 -m 3 -y 10 -t 250000 -r 36
+ContGenAdm.exe add -i 10005 -n 20 -m 3 -y 10 -t 250000 -r 37
+ContGenAdm.exe add -i 10006 -n 20 -m 3 -y 10 -t 250000 -r 68
+
+ContGenAdm.exe add -i 9018 -n 20 -m 3 -y 10 -t 250000 -r 18
+ContGenAdm.exe add -i 9004 -n 20 -m 3 -y 10 -t 250000 -r 29
+ContGenAdm.exe add -i 9000 -n 20 -m 3 -y 10 -t 250000 -r 29
+
+
+ContGenAdm.exe add -i 9035 -n 20 -m 3 -y 10 -t 250000 -r 19
+ContGenAdm.exe add -i 9042 -n 20 -m 3 -y 10 -t 250000 -r 43

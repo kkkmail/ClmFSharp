8e8bd2314fdde9bc99aa18161412855200150dcf
kkkmail <kkk@jfcglobal.net>
2020-06-09 21:08:23 -0400

    FSV:4.0000, V:5.0.3

--- Clm/ClmDefaults/DefaultValues/004/Defaults_004_002_000.fs
+++ Clm/ClmDefaults/DefaultValues/004/Defaults_004_002_000.fs
@@ -7,51 +7,52 @@ open Clm.Distributions
 open ClmSys.ContGenPrimitives
 
 module Defaults_004_002_000 =
+    let sdSim = 0.1
 
-    let nsd =
+    let nSim =
         [
-            0.00
-            0.10
-            //0.20
-            //0.30
-            //0.40
-            //0.50
-            //0.60
-            //0.70
-            //0.80
-            //0.90
-            //1.00
+            0.0000
+            0.0010
+            0.0020
+            0.0030
+            0.0050
+            0.0070
+            0.0100
+            0.0130
+            0.0160
+            0.0200
         ]
         |> withRowNumber
 
 
-    let mcl =
+    let mScMult =
         [
-            (0.000_000_100, 2_000.0)
-            (0.000_000_200, 2_000.0)
-            (0.000_000_500, 2_000.0)
-            (0.000_001_000, 2_000.0)
-            (0.000_002_000, 2_000.0)
-            (0.000_005_000, 2_000.0)
-            (0.000_010_000, 2_000.0)
-            (0.000_020_000, 2_000.0)
-            (0.000_050_000, 2_000.0)
+            (0.0,                 0.0)
+            (0.000_000_100, 100_000.0)
+            (0.000_000_200, 100_000.0)
+            (0.000_000_500, 100_000.0)
+            (0.000_001_000, 100_000.0)
+//            (0.000_002_000, 100_000.0)
+//            (0.000_005_000, 100_000.0)
+//            (0.000_010_000, 100_000.0)
+//            (0.000_020_000, 100_000.0)
+//            (0.000_050_000, 100_000.0)
         ]
 
-        // Remove once no longer needed.
-        // For N = 3
-        |> List.map (fun (a, b) -> 1000.0 * a, b)
-
+//        // Remove once no longer needed.
+//        // For N = 3
+//        |> List.map (fun (a, b) -> 1000.0 * a, b)
+//
 //        // For N = 2
 //        |> List.map (fun (a, b) -> 2500.0 * a, b)
 
-
-        |> List.map (fun (a, b) -> Some a, b)
-        |> withRowNumber
+      |> withRowNumber
 
 
-    let getDefaultValue ((n, s), (m, (a, b))) =
+    let getDefaultValue ((n, similarity), (m, (scarcity, multiplier))) =
         let clmDefaultValueId = (4_002_000_000L + 20L * m + n) |> ClmDefaultValueId
+        printfn "clmDefaultValueId = %A, similarity = %A, scarcity = %A, multiplier = %A" clmDefaultValueId similarity scarcity multiplier
+
         let description = None
         let catRateGenType = ByEnantiomerPairs FixedVal
         let successNumberType = ThresholdBased
@@ -62,30 +63,37 @@ module Defaults_004_002_000 =
             //===========================================================
             let synthParam = ReactionRateProviderParams.defaultSynthRndParamImpl (Some 0.001, None)
             let catSynthRndParam = (synthParam, (Some 0.000_100), 100_000.0)
-            let catSynthParam = ReactionRateProviderParams.defaultCatSynthSimParam catSynthRndParam (Some s) catRateGenType
+
+            let catSynthParam =
+                ReactionRateProviderParams.defaultCatSynthSimParam catSynthRndParam (Some sdSim) catRateGenType
             //===========================================================
             let destrParam = ReactionRateProviderParams.defaultDestrRndParamImpl (Some 0.001, None)
 
-//            let catDestrRndParam = (destrParam, (Some 0.000_100), 100_000.0)
-            let catDestrRndParam = (destrParam, (Some 0.30), 100_000.0)
+            let catDestrRndParam = (destrParam, (Some 0.000_100), 100_000.0)
 
-            let catDestrParam = ReactionRateProviderParams.defaultCatDestrSimParam catDestrRndParam (Some s) catRateGenType
+//            // For n = 2 - 3. Remove once no longer needed.
+//            let catDestrRndParam = (destrParam, (Some 0.30), 100_000.0)
+
+            let catDestrParam =
+                ReactionRateProviderParams.defaultCatDestrSimParam catDestrRndParam (Some sdSim) catRateGenType
             //===========================================================
-            let ligParam = ReactionRateProviderParams.defaultLigRndParamImpl (1.0, 1.0)
-            let catLigParam = ReactionRateProviderParams.defaultCatLigSimParam (ligParam, a, b) (Some 0.3) catRateGenType
+            let ligParam = ReactionRateProviderParams.defaultLigRndParamImpl (0.001, 0.001)
+
+            let catLigParam =
+                ReactionRateProviderParams.defaultCatLigSimParam (ligParam, Some scarcity, multiplier) (Some similarity) catRateGenType
             //===========================================================
             let rates =
                 [
 //                    wasteRecyclingParam
 //
-//                    synthParam |> SynthesisRateParam
+                    synthParam |> SynthesisRateParam
 //                    catSynthParam
 //
 //                    destrParam |> DestructionRateParam
 //                    catDestrParam
 
                     ligParam |> LigationRateParam
-                    catLigParam
+                    if (scarcity > 0.0 && multiplier > 0.0) then catLigParam
                 ]
             //===========================================================
 
@@ -100,4 +108,4 @@ module Defaults_004_002_000 =
             description = description
         }
 
-    let defaultValues = (List.allPairs nsd mcl) |> List.map getDefaultValue
+    let defaultValues = (List.allPairs nSim mScMult) |> List.map getDefaultValue

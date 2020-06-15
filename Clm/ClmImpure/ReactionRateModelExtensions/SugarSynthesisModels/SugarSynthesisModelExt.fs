38f880cf37383c64f4d7aef91f2a9794262975f5
kkkmail <kkk@jfcglobal.net>
2020-06-15 07:27:04 -0400

    FSV:5.0, V:6.0.0

---
+++ Clm/ClmImpure/ReactionRateModelExtensions/SugarSynthesisModels/SugarSynthesisModelExt.fs
@@ -0,0 +1,17 @@
+﻿namespace ClmImpure.ReactionRateModelExtensions
+
+open ClmImpure.ReactionRateModels
+open ClmImpure.ReactionRateModels.SugarSynthesisModel
+
+module SugarSynthesisModelExt =
+
+    type SugarSynthesisModel
+        with
+
+        static member modelGetter (p : ReactionRateModelWithUsage) =
+            match p.model with
+            | SugarSynthesisRateModel d -> Some d
+            | _ -> None
+
+
+        static member tryCreate (p, m) = (p, m) |> SynthesisRandomModel.tryCreate

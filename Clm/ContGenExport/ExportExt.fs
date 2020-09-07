eca4e093278c71d126d03e16d348e94f8bda9aa1
kkkmail <kkk@jfcglobal.net>
2020-09-07 19:00:48 -0400

    FSV:5.0, V:6.0.0.4

---
+++ Clm/ContGenExport/ExportExt.fs
@@ -0,0 +1,25 @@
+﻿namespace ContGenExport
+
+open ClmSys.Export
+open Clm.ModelParams
+open Clm.ReactionRates
+
+module ExportExt =
+    
+    type ReactionRateProviderParams
+        with
+        
+        member p.export() =
+            [ "", "" ]
+    
+    
+    type ClmDefaultValue
+        with
+        
+        member x.export() =
+            0
+            
+            
+    let x (d : list<ClmDefaultValue>) =
+        d |> List.map (fun e -> e)
+            
